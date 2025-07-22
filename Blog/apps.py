7555bf48554055d777bccc130acdff47f4c2d877
Akshat Swami <akshatswami0610@gmail.com>
2025-07-22 17:27:45 +0530

    removed

--- Blog/apps.py
+++ Blog/apps.py
@@ -7,17 +7,4 @@ class BlogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Blog'
 
-    def ready(self):
-        # Auto-create superuser if none exists
-        try:
-            User = get_user_model()
-            if not User.objects.filter(is_superuser=True).exists():
-                User.objects.create_superuser(
-                    username='Akshat',
-                    email='akshatswami0610@gmail.com',
-                    password='123'
-                )
-                print("Superuser created: Akshat / 123")
-        except OperationalError:
-            # Database might not be ready on first run
-            pass
+    
